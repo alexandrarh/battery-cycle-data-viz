87bf42fc26ecce9292816d80a74c2865c5343f18
alex hernandez <88351332+alexavndra@users.noreply.github.com>
2024-03-02 22:34:20 -0800

    Update 1.1; see release for more info

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import streamlit as st
+from pathlib import Path
 
 # Config for page
 menu_items_page = {"Report a bug":"mailto:alexavndrarh@gmail.com","About":"This is version **1.0**"}
@@ -25,8 +26,7 @@ st.write("""
 )
 st.subheader("Requirements from you")
 st.write("""
-- Make sure all columns are formatted properly
-- You're uploading a .csv file (Can use Excel to do this or any online converter)
+- The file you give is either a .csv, .xls, or .xlsx file
 
 **NOTE**: Only works for battery data generated from Neware (as of right now)!
 """
@@ -34,9 +34,9 @@ st.write("""
 
 # User configurations
 st.header("Your configurations here")
-battery_file = st.file_uploader(label="Upload your CSV here", type="csv")
+battery_file = st.file_uploader(label="Upload your CSV here", type=["csv", "xls", "xlsx"])
 if battery_file is None:
-    st.error("Please upload a CSV file")
+    st.error("Please upload a CSV, XLS, or XLSX file")
     st.stop()
 
 cycles_to_generate = st.text_input(label="Type cycles to generate; leave empty to generate all", placeholder="Type in this format: 1,5,10")
@@ -48,6 +48,9 @@ battery_df_file_name = battery_df_file_name_input + ".png"
 
 generate_button = st.button(label="Generate visualization", type="primary")
 
+'''
+
+'''
 # Functions to reduce clutter
 def drop_unwanted_cycles(battery_df, cycles_to_generate):
     if cycles_to_generate:
@@ -82,12 +85,38 @@ def plotting_battery_cycle(charge, discharge, min_val, max_val, cycle_legend, co
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
+def standardize_df(battery_df):
+    battery_df.columns = [
+        "Data serial number", "Cycle ID", "Step ID", "Step number", "Step Type", "Time(hh:mm:ss)", "Total time(hh:mm:ss)",
+        "Current(mA)", "Voltage(V)", "Capacity(mAh)", "Specific Capacity(mAh/g)", "Charge Cap.(mAh)", "Charging capacity(mAh/g)",
+        "DChg cap.(mAh)", "Discharge specific capacity(mAh/g)", "Energy(Wh)", "Specific energy(mWh/g)", "Chg Eng.(Wh)",
+        "Charge ratio energy(mWh/g)", "Discharge energy(Wh)",
+        "Discharge specific energy(mWh/g)", "Real time","Power(W)"
+        ]
+    battery_df = battery_df.iloc[1:]
+    battery_df.reindex()
+    for column in battery_df.columns.to_list():
+        if column == "Cycle ID":
+            battery_df[column] = battery_df[column].astype(int)
+        elif column == "Voltage(V)" or column == "Current(mA)" or column == "Specific Capacity(mAh/g)":
+            battery_df[column] = battery_df[column].astype(float)
+    return battery_df
+        
 # Plot generation here
 if generate_button and battery_file:
     # Setup pre-plot
-    battery_df = pd.read_csv(battery_file)
+    # Create if-statement to evaluate what the file is (excel or csv)
+    # If .xls, then run a function that converts it and standardizes it
+    if battery_file.name.endswith(".csv"):
+        battery_df = pd.read_csv(battery_file)
+    elif battery_file.name.endswith(".xls") or battery_file.name.endswith(".xlsx"):
+        battery_df = pd.read_excel(battery_file,sheet_name="Record")
 
-    # When there are args in cycles_to_generate   
+    # When you need to standardize the columns
+    if battery_df.columns.values[0] != "Data serial number":
+        battery_df = standardize_df(battery_df)
+    
+    # When there are args in cycles_to_generate 
     battery_df = drop_unwanted_cycles(battery_df, cycles_to_generate)
 
     number_of_colors = battery_df["Cycle ID"].nunique()
@@ -106,5 +135,7 @@ if generate_button and battery_file:
     max_val = battery_df["Voltage(V)"].max()
 
     battery_cycle_viz = plotting_battery_cycle(charge, discharge, min_val, max_val, cycle_legend, color_range, plot_title)
+    # img = plt.savefig(battery_df_file_name)
     st.pyplot(battery_cycle_viz)
-    st.download_button(label="Download generated cycle data",data=convert_df(battery_df),file_name="generated-battery-df.csv")
+    st.download_button(label="Download generated cycle data", data=convert_df(battery_df), file_name="generated-battery-df.csv")
+    # st.download_button(label="Download generated visualization", data=img,file_name=battery_df_file_name)
